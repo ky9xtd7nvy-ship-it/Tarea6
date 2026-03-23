6eb0f208475eff76b9ee1c398a4f052f20d4cd36
ky9xtd7nvy-ship-it <ky9xtd7nvy@privaterelay.appleid.com>
2026-03-23 21:04:06 +0100

    Interface creada para recibir datos de usuarios y servicio para interactuar con los datos de estos desde la base de datos

--- src/app/interfaces/usuario.interface.ts
+++ src/app/interfaces/usuario.interface.ts
@@ -1,1 +1,17 @@
-export interface Usuario {}
+export interface Usuario {
+  id?: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  email: string;
+  image: string;
+  password?: string; // Solo para el formulario de alta
+}
+
+export interface RespuestaUsuarios {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: Usuario[]; // Aquí mapeamos el array 'data' de tu JSON
+}
--- src/app/services/usuarios.ts
+++ src/app/services/usuarios.ts
@@ -1,6 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Usuario, RespuestaUsuarios } from '../interfaces/usuario.interface';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Usuarios {}
+export class UsuariosService {
+  private _http = inject(HttpClient);
+  private _urlBase = 'https://peticiones.online';
+
+  listaUsuarios = signal<Usuario[]>([]);
+
+  async obtenerTodos() {
+    try {
+      const respuesta = await firstValueFrom(this._http.get<RespuestaUsuarios>(this._urlBase));
+      this.listaUsuarios.set(respuesta.data);
+    } catch (error) {
+      console.error('Error al cargar usuarios', error);
+    }
+  }
+
+  obtenerPorId(id: string): Observable<Usuario> {
+    return this._http.get<Usuario>(`${this._urlBase}/${id}`);
+  }
+
+  crear(usuario: Usuario): Observable<Usuario> {
+    return this._http.post<Usuario>(this._urlBase, usuario);
+  }
+
+  actualizar(usuario: Usuario): Observable<Usuario> {
+    return this._http.put<Usuario>(`${this._urlBase}/${usuario.id}`, usuario);
+  }
+
+  borrar(id: number): Observable<any> {
+    return this._http.delete<any>(`${this._urlBase}/${id}`);
+  }
+}
