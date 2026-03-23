2cc3ffb27a1796549a5ab5816fab663f1ee0bfd4
ky9xtd7nvy-ship-it <ky9xtd7nvy@privaterelay.appleid.com>
2026-03-23 21:34:25 +0100

    Integración de cabecera y router-outlet en el componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cabecera } from './components/cabecera/cabecera';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Cabecera],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Tarea6');
-}
+export class AppComponent {}
--- src/app/components/cabecera/cabecera.ts
+++ src/app/components/cabecera/cabecera.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cabecera',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './cabecera.html',
-  styleUrl: './cabecera.css',
+  styleUrl: './cabecera.css'
 })
 export class Cabecera {}
